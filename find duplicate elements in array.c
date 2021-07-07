36d6ab96b7c606a61ba25bd68d21cb7b03e35b15
Mahudoom <66937573+easa2000@users.noreply.github.com>
2021-07-07 23:45:27 +0530

    Create find duplicate elements in array.c

---
+++ find duplicate elements in array.c
@@ -0,0 +1,23 @@
+// #include<stdio.h>
+// void main(){
+//     int n,i,arr[20],j,flag=0;
+//     printf("\n Enter the size of the array: ");
+//     scanf("%d",&n);
+//     printf("\n Enter the elements: ");
+//     for(i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     // checking
+
+//     for(i=0;i<n;i++){
+//         for(j=i+1;j<n;j++){
+//         if(arr[i] == arr[j] && i != j){
+//             flag = 1;
+//             printf("\n Duplicate numbers found at location %d and %d",i,j);
+//         }
+//         }
+//     }
+//     if(flag == 0)
+//     printf("\n No Duplicate");
+// }
+
